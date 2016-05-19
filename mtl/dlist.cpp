d4fa19d2600802c1bacab3bf15198880954845cc
mrsep <hans.peschke@tongguantech.com>
2016-05-19 13:08:13 +0800

    update mtl/dlist
    Signed-off-by: mrsep <hans.peschke@tongguantech.com>

--- mtl/dlist.cpp
+++ mtl/dlist.cpp
@@ -1,19 +1,66 @@
 #include "dlist.h"
 #include <iostream>
+#include <list>
+#include <vector>
+#include <memory>
+
+void test_next(const std::unique_ptr<A>& a) {
+  A* i = a.get();
+  while (i != nullptr) {
+    std::cout << i->x << "\t";
+    i = i->Next();
+  }
+  std::cout << std::endl;
+}
+
+void test_prev(const std::unique_ptr<A>& a) {
+  A* i = a.get();
+  while (i != nullptr) {
+    std::cout << i->x << "\t";
+    i = i->Prev();
+  }
+  std::cout << std::endl;
+}
+
+void test_dlist(const std::list<std::unique_ptr<A>>& v) {
+  if (!v.empty()) {
+    auto& f = v.front();
+    test_next(f);
+    auto& b = v.back();
+    test_prev(b);
+  }
+}
 
 int main() {
 
-  A a(1,1);
-  A b(2,2);
-  A c(3,3);
-  A d(4,4);
+  A* a = new A(0,0);
+  std::cout << sizeof(a) << std::endl;
+  std::list<std::unique_ptr<A>> v;
 
-  std::cout << a.x << std::endl;
-  std::cout << a.Next().Next().x << std::endl;
-  b.~A();
-  std::cout << a.Next().Next().x << std::endl;
-  std::cout << a.Next().Prev().x << std::endl;
+  v.push_back(std::unique_ptr<A>(new A{1,1}));
+  v.push_back(std::unique_ptr<A>(new A{2,2}));
+  v.push_back(std::unique_ptr<A>(new A{3,3}));
+  v.push_back(std::unique_ptr<A>(new A{4,4}));
+  A* b = new A(10,10);
+  test_dlist(v);
 
+  std::cout << "cleanup" << std::endl;
+  v.pop_front();
+  v.pop_back();
+  //v.pop_back();
+  v.pop_front();
+
+  std::cout << "dlist cleanup" << std::endl;
+  //delete a; delete b;
+  
+  delete a->Next();
+  //delete A::Head();
+  delete A::Tail();
+
+
+  test_dlist(v);
+
+  std::cout << "automatic cleanup" << std::endl;
   return 0;
 }
 
--- mtl/dlist.h
+++ mtl/dlist.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <iostream>
 
 template <typename T>
 class dlist {
@@ -13,9 +12,8 @@ private:
 
 private:
   // helper functions
-  bool inline is_head() { return prev == nullptr; }
-  bool inline is_tail() { return next == nullptr; }
-  bool inline is_empty() { return head == nullptr && tail == nullptr; }
+  inline bool is_head() { return prev == nullptr; }
+  inline bool is_tail() { return next == nullptr; }
 
 public:
   // ctor & dtor
@@ -31,6 +29,7 @@ public:
       prev = tail;
       tail = T_this;
     }
+    std::cout << "dlist ctor called" << std::endl;
   }
 
   virtual ~dlist() {
@@ -38,18 +37,24 @@ public:
     else { prev->next = next; }
     if (is_tail()) { tail = prev; }
     else { next->prev = prev; }
+    std::cout << "dlist dtor called" << std::endl;
   }
 
 // public interface
 public:
-  T& Next() { return *next; }
-  T& Prev() { return *prev; }
-  T& Head() { return *head; }
-  T& Tail() { return *tail; }
+  inline T* Next() { return next; }
+  inline T* Prev() { return prev; }
+  static inline T* Head() { return head; }
+  static inline T* Tail() { return tail; }
+  static inline bool is_empty() { return head == nullptr && tail == nullptr; }
 
   // iterator
+
+  // what about other constructors, operators?
 };
 
+
+// example type
 struct A : public dlist<A> {
 
   int x,y;
