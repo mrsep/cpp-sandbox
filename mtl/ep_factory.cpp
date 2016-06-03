6e313f8fdb3a85a2f04befd4f158f1a3a413f84a
Hans Peschke <sep@guug.de>
2016-06-03 14:53:11 +0800

    add predicated Fill functor to ep_factory
    Signed-off-by: Hans Peschke <sep@guug.de>

--- mtl/ep_factory.cpp
+++ mtl/ep_factory.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <memory>
 #include <list>
+#include <set>
+
+enum class EP { BLINK, HEART };
 
 struct EventProvider {
   EventProvider() = default;
@@ -11,6 +14,8 @@ struct EventProvider {
 
   virtual void update(float) = 0;
   virtual float read() = 0;
+
+  const static EP epkind;
 };
 
 struct Config {
@@ -32,6 +37,7 @@ struct BlinkProvider : public EventProvider, public dlist<BlinkProvider> {
   BlinkProvider(BlinkProvider&& other) : pata(other.pata), data(other.data) {}
   BlinkPata pata;
   BlinkData data;
+  const static EP epkind;
 
   void update(float state) override { data.current_state = state; }
   float read() override { return pata.pata + data.current_state; }
@@ -43,9 +49,22 @@ struct HeartProvider : public EventProvider, public dlist<HeartProvider> {
   HeartProvider(HeartProvider&& other) : pata(other.pata), data(other.data) {}
   BlinkPata pata;
   BlinkData data;
+  const static EP epkind;
 
   void update(float state) override { data.current_state = state; }
   float read() override { return pata.pata + data.current_state; }
+};
+
+const EP BlinkProvider::epkind = EP::BLINK;
+const EP HeartProvider::epkind = EP::HEART;
+
+template <typename T, typename Container, typename Conf, typename Pred>
+struct Fill_pred {
+  void operator()(Container& container, const Conf& config, const Pred& pred) {
+    if (pred.count(T::epkind) > 0 ) {
+      container.emplace_back(typename Container::value_type(new T(config)));
+    }
+  }
 };
 
 template <typename T, typename Container, typename Conf>
@@ -55,31 +74,57 @@ struct Fill {
   }
 };
 
+template <typename T>
+struct Clear {
+  void operator()() {
+    T::Clear();
+  }
+};
+
+std::set<EP> eps{EP::BLINK};
+
 int main() {
   using ep_tlist = type_list<HeartProvider, BlinkProvider>;
   using ptr_t = EventProvider*;
   //using ptr_t = std::unique_ptr<EventProvider>;
   using ep_container = std::list<ptr_t>;
+  using ep_pred = std::set<EP>;
 
   ep_container provider;
 
   //provider.push_back(ptr_t(new HeartProvider(Config{true})));
-  Fill<HeartProvider,ep_container,Config>()(provider, Config{true});
+  //Fill<HeartProvider,ep_container,Config>()(provider, Config{true});
+
+  Config ca{true};
+  Config cb{false};
   
   for (int i=0; i < 1000; ++i) {
-    //provider.clear();
-
-    for_each_type<ep_tlist,Fill,ep_container,Config>(provider, Config{true});
-    for_each_type<ep_tlist,Fill,ep_container,Config>(provider, Config{false});
+    for_each_type<ep_tlist,Fill,ep_container,Config>(provider, ca);
+    for_each_type<ep_tlist,Fill,ep_container,Config>(provider, cb);
   }
 
-  std::cout << provider.size() << std::endl;
-  for (auto& ep : provider) {
-    std::cout << ep->read() << std::endl;
+  std::cout << "providers: " << provider.size() << std::endl;
+  std::cout << "BP: " << BlinkProvider::Size() << std::endl;
+  std::cout << "HP: " << HeartProvider::Size() << std::endl;
+
+  for (int i=0; i < 1000; ++i) {
+    for_each_type<ep_tlist,Fill,ep_container,Config>(provider, ca);
+    for_each_type<ep_tlist,Fill,ep_container,Config>(provider, cb);
   }
 
-  BlinkProvider::Clear();
+  provider.clear();
+  for_each_type<ep_tlist,Clear>();
 
+  std::cout << "providers: " << provider.size() << std::endl;
+  std::cout << "BP: " << BlinkProvider::Size() << std::endl;
+  std::cout << "HP: " << HeartProvider::Size() << std::endl;
+
+  for (int i=0; i < 1000; ++i) {
+    for_each_type<ep_tlist,Fill_pred,ep_container,Config,ep_pred>(provider, ca, eps);
+    for_each_type<ep_tlist,Fill_pred,ep_container,Config,ep_pred>(provider, cb, eps);
+  }
+
+  std::cout << "providers: " << provider.size() << std::endl;
   std::cout << "BP: " << BlinkProvider::Size() << std::endl;
   std::cout << "HP: " << HeartProvider::Size() << std::endl;
 
